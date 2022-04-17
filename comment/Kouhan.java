124b2f8391a18292a06327fea476c98aa7dd1aeb
fu-kunn <sky53908@gmail.com>
2022-04-17 08:56:10 +0000

    [6-2]練習問題

---
+++ comment/Kouhan.java
@@ -0,0 +1,12 @@
+package comment;
+
+public class Kouhan {
+  public static void callDeae() {
+    System.out.println("小癪な");
+  }
+  public static void showMondokoro() throws Exception {
+    System.out.println("角さん");
+    System.out.println("この紋所");
+    Zenhan.doTogame();
+  }  
+}
